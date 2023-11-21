5dd8fdba82514a9640aa59d0dd30e338457e0e26
Maxime Vernoux <mxm.vernoux@gmail.com>
2023-11-21 11:13:12 +0100

    Update clientsFunction.js

--- public/assets/js/GestionClient/clientsFunction.js
+++ public/assets/js/GestionClient/clientsFunction.js
@@ -1,13 +1,13 @@
 function chercheUn() {
-    if(document.getElementById("titre").value == "-- Choisir --") { //ce test permet d'eviter l'envoie d'une erreur si nous choisison l'element de base.
+    if (document.getElementById("titre").value == "-- Choisir --") { //ce test permet d'eviter l'envoie d'une erreur si nous choisison l'element de base.
         document.getElementById("retour").style.visibility = "hidden";
     } else {
         var xhr = new XMLHttpRequest()
         var reponse;
         xhr.onreadystatechange() = function(){
-            if(this.readyState == 4 && this.status == 200) {
+            if (this.readyState == 4 && this.status == 200) {
                 document.getElementById('retour').innerHTML = xhr.responseText;
-                document.getElementById("retour").style.visibility="visible";
+                document.getElementById("retour").style.visibility = "visible";
             }
         }
         xhr.open("post", "/?c=gestionClient&a=chercheUnAjax", true);
@@ -15,6 +15,6 @@ function chercheUn() {
         parametres = "id=" + document.getElementById("titre").value;
         xhr.send(parametres);
     }
-
+}
 var chIdClient = document.getElementById("titre");
 chIdClient.addEventListener('change', chercheUn, false);
